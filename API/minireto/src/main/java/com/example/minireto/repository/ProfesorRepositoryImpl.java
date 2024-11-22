c5e86461065ce2bf834cb34802af2df3c9919812
Kangelx <angel.garciagarcia@iesmiguelherrero.com>
2024-11-22 20:12:52 +0100

    #2 creacion estructura profesor

--- API/minireto/src/main/java/com/example/minireto/repository/ProfesorRepositoryImpl.java
+++ API/minireto/src/main/java/com/example/minireto/repository/ProfesorRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.example.minireto.repository;
 
+import com.example.minireto.ENUMS.GENERO;
 import com.example.minireto.model.Profesor;
 import com.example.minireto.repository.interfaces.ProfesorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -18,31 +23,66 @@ public class ProfesorRepositoryImpl implements ProfesorRepository {
 
     @Override
     public List<Profesor> findAll() {
-        return List.of();
+        return jdbcTemplate.query(" SELECT * FROM profesores", new ProfesorRowWrapper());
     }
 
     @Override
     public Profesor findById(String id) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM profesores where idprofesor = ?", new ProfesorRowWrapper(), id);
     }
 
     @Override
     public Profesor findByDni(String dni) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM profesores where dni = ?", new ProfesorRowWrapper(), dni);
     }
 
     @Override
     public int save(Profesor profesor) {
-        return 0;
+        String sql = "INSERT INTO profesores (nombre, apellidos, dni, email, password_encr, telefono, genero, fechaNac, especialidad, activo, admin) "+
+                "Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        return jdbcTemplate.update(sql, profesor.getNombre(), profesor.getApellidos(), profesor.getDni(), profesor.getEmail(), profesor.getPassword_encr(), profesor.getTelefono(), profesor.getGenero(), profesor.getFechaNac(), profesor.getEspecialidad(), profesor.isActivo(), profesor.isAdmin());
     }
 
     @Override
     public int update(Profesor profesor) {
-        return 0;
+        try{
+            return jdbcTemplate.update(
+                    "UPDATE profesores SET nombre = ?, apellidos = ?, dni = ?, email = ?, password_encr = ?, telefono = ?, genero = ?, fechaNac = ?, especialidad = ?, activo = ?, admin = ? " +
+                            " WHERE idProfesor = ?",
+                    profesor.getNombre(), profesor.getApellidos(), profesor.getDni(), profesor.getEmail(), profesor.getPassword_encr(), profesor.getTelefono(), profesor.getGenero(), profesor.getFechaNac(), profesor.getEspecialidad(), profesor.isActivo(), profesor.isAdmin(), profesor.getIdprofesor()
+
+            );
+        }catch (Exception e){
+            return -1;
+        }
     }
 
     @Override
     public int deleteById(String id) {
-        return 0;
+        return jdbcTemplate.update("DELETE FROM profesores WHERE idprofesor = ?", id);
+    }
+
+
+    private static class ProfesorRowWrapper implements RowMapper<Profesor> {
+
+        @Override
+        public Profesor mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Profesor profesor = new Profesor();
+
+            profesor.setNombre(rs.getString("nombre"));
+            profesor.setApellidos(rs.getString("apellidos"));
+            profesor.setDni(rs.getString("dni"));
+            profesor.setEmail(rs.getString("email"));
+            profesor.setPassword_encr(rs.getString("password_encr"));
+            profesor.setTelefono(rs.getString("telefono"));
+            profesor.setGenero(GENERO.valueOf(rs.getString("genero")));
+            profesor.setFechaNac(rs.getDate("fechaNac"));
+            profesor.setEspecialidad(rs.getString("especialidad"));
+            profesor.setActivo(rs.getBoolean("activo"));
+            profesor.setAdmin(rs.getBoolean("admin"));
+
+            return profesor;
+        }
     }
 }
